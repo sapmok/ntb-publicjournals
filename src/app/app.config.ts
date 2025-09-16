abb54e344f1e57315b3890717c29b55e4fbb326a
ProkhorenkovAG <prohorenkovag@ntckompas.ru>
2025-09-16 13:00:25 +0200

    Переделал метрику

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,11 @@
 import {
   ApplicationConfig,
-  inject,
-  NgZone,
   provideBrowserGlobalErrorListeners,
-  provideEnvironmentInitializer,
   provideZoneChangeDetection,
 } from '@angular/core';
-import {
-  NavigationEnd,
-  provideRouter,
-  Router,
-  withHashLocation,
-  withInMemoryScrolling,
-} from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
-import { YandexMetrikaService } from './services/yandex-metrika.service';
-import { environment } from './environments/environment';
-import { filter } from 'rxjs/operators';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -31,23 +19,5 @@ export const appConfig: ApplicationConfig = {
         anchorScrolling: 'disabled',
       }),
     ),
-    provideEnvironmentInitializer(() => {
-      const ym = inject(YandexMetrikaService);
-      const router = inject(Router);
-      const zone = inject(NgZone);
-
-      ym.init(environment.metrikaCounterId, {
-        clickmap: true,
-        trackLinks: true,
-        accurateTrackBounce: true,
-        webvisor: environment.production,
-      });
-
-      zone.runOutsideAngular(() => {
-        router.events
-          .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
-          .subscribe((e) => ym.hit(e.urlAfterRedirects));
-      });
-    }),
   ],
 };
--- src/app/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  metrikaCounterId: 104165537,
-};
